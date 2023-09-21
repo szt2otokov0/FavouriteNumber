22f1cff540a80aba4b3b7a469626fcb60fe5f756
szt2otokov <szt2otokov@vasvari.org>
2023-09-21 12:45:37 +0200

    dun

---
+++ src/FavouriteNumber.java
@@ -0,0 +1,6 @@
+public class FavouriteNumber {
+    public static void main(String[] args) {
+        int favouriteNumber = 4;
+        System.out.printf("my favourite number is %d%n", favouriteNumber);
+    }
+}
